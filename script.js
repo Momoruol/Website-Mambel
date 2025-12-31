b891421de33d6eb8071e6ebee5a22caf08b0e438
Joao Eduardo Notebook Momo <dudu280325@gmail.com>
2025-12-31 15:42:28 -0300

    Melhorias de UI UX design para a sidebar

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const btn = document.getElementById('menu-btn')
 const menu = document.getElementById('menu')
+const body = document.querySelector('body')
+const backdrop = document.getElementById('backdrop')
 
 btn.addEventListener('click', navToggle)
+backdrop.addEventListener('click', sideClose)
 
 function navToggle() {
   btn.classList.toggle('open')
   menu.classList.toggle('open')
+  body.classList.toggle('no-scroll')
+}
+
+function sideClose(){
+  btn.classList.remove('open')
+  menu.classList.remove('open')
+  body.classList.remove('no-scroll')
 }
