67e4c98410d37164a1c42f2266104829c227f506
João Eduardo <joaoeduardo@hettech.com.br>
2026-06-02 23:06:25 -0300

    Atualização para inclusão de zoom dinamico no catalogo

--- script_page-flip.js
+++ script_page-flip.js
@@ -99,3 +99,89 @@ function fixCanvasDPR() {
   ctx.setTransform(dpr, 0, 0, dpr, 0, 0);
 }
 
+const imagensCatalogo = new Array(
+  "./catalogo/page_1.jpg",
+  "./catalogo/page_2.jpg",
+  "./catalogo/page_3.jpg",
+  "./catalogo/page_4.jpg",
+  "./catalogo/page_5.jpg",
+  "./catalogo/page_6.jpg",
+  "./catalogo/page_7.jpg",
+  "./catalogo/page_8.jpg",
+  "./catalogo/page_9.jpg",
+  "./catalogo/page_10.jpg",
+  "./catalogo/page_11.jpg",
+  "./catalogo/page_12.jpg",
+  "./catalogo/page_13.jpg",
+  "./catalogo/page_14.jpg",
+  "./catalogo/page_15.jpg",
+  "./catalogo/page_16.jpg",
+  "./catalogo/page_17.jpg",
+  "./catalogo/page_18.jpg",
+  "./catalogo/page_19.jpg",
+  "./catalogo/page_20.jpg",
+  "./catalogo/page_21.jpg",
+  "./catalogo/page_22.jpg",
+  "./catalogo/page_23.jpg",
+  "./catalogo/page_24.jpg",
+  "./catalogo/page_25.jpg",
+  "./catalogo/page_26.jpg",
+  "./catalogo/page_27.jpg",
+  "./catalogo/page_28.jpg",
+  "./catalogo/page_29.jpg",
+  "./catalogo/page_30.jpg",
+  "./catalogo/page_31.jpg",
+  "./catalogo/page_32.jpg",
+  "./catalogo/page_33.jpg",
+  "./catalogo/page_34.jpg",
+  "./catalogo/page_35.jpg",
+  "./catalogo/page_36.jpg",
+  "./catalogo/page_37.jpg",
+  "./catalogo/page_38.jpg",
+  "./catalogo/page_39.jpg",
+  "./catalogo/page_40.jpg",
+  "./catalogo/page_41.jpg",
+  "./catalogo/page_42.jpg",
+  "./catalogo/page_43.jpg",
+  "./catalogo/page_44.jpg",
+  "./catalogo/page_45.jpg",
+  "./catalogo/page_46.jpg",
+  "./catalogo/page_47.jpg",
+  "./catalogo/page_48.jpg",
+  "./catalogo/page_49.jpg",
+  "./catalogo/page_50.jpg",
+  "./catalogo/page_51.jpg",
+  "./catalogo/page_52.jpg"
+)
+const btnLeitura = document.getElementById('btn-leitura');
+const overlayLeitura = document.getElementById('overlay-leitura');
+const btnFechar = document.getElementById('btn-fechar-leitura');
+const imgLeitura = document.getElementById('img-leitura');
+
+// Ativa o Modo Leitura
+btnLeitura.addEventListener('click', () => {
+  // Pega o índice da página atual (0, 1, 2...)
+  const paginaAtual = pageFlip.getCurrentPageIndex();
+  
+  // Injeta a imagem correta na tag <img>
+  imgLeitura.src = imagensCatalogo[paginaAtual];
+  
+  // Mostra o overlay removendo a classe 'hidden' do Tailwind
+  overlayLeitura.classList.remove('hidden');
+  overlayLeitura.classList.add('flex');
+  
+  // Trava o scroll do body para o usuário não rolar o site por trás do modal
+  document.body.style.overflow = 'hidden';
+});
+
+// Desativa o Modo Leitura
+btnFechar.addEventListener('click', () => {
+  overlayLeitura.classList.add('hidden');
+  overlayLeitura.classList.remove('flex');
+  
+  // Libera a memória limpando o source
+  imgLeitura.src = ''; 
+  
+  // Destrava o scroll do body
+  document.body.style.overflow = '';
+});
